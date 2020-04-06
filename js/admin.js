ea8e398e79a4a6058e5498cc22a90da0124e01f3
jhony2703 <jc.pv.27@gmail.com>
2020-04-06 17:08:39 -0500

    z
    z

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,9 @@
+document.getElementById('btn-menu').addEventListener('click', muestraMenu)
 var visible_panel = 0
-document.getElementById('btn-menu').addEventListener('click', function(e){
-    
+
+function muestraMenu(e){
+    e.preventDefault()
     const panel = document.getElementById('panel')
-    
     if(visible_panel===0)
     {
         panel.style.visibility='hidden'
@@ -10,14 +11,12 @@ document.getElementById('btn-menu').addEventListener('click', function(e){
         visible_panel=1
     }
     else{
-        
         panel.style.visibility='visible'
         panel.style.opacity = '1'
         visible_panel=0
     }
+
+    
     panel.style.transition= 'ease 0.5s all'
-
-})
-
-
+}
 
