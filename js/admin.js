242cbaade6d21a3a951c0b08f306ac36a9e7d4b0
jhony2703 <jc.pv.27@gmail.com>
2020-04-06 16:41:39 -0500

    a
    a

--- js/admin.js
+++ js/admin.js
@@ -1,24 +1,24 @@
 var visible_panel = 0
 document.getElementById('btn-menu').addEventListener('click', function(e){
-    e.preventDefault(false)
+    
     const panel = document.getElementById('panel')
+    panel.style.display='inline'
     if(visible_panel===0)
     {
-        panel.style.display='none'
         panel.style.visibility='hidden'
         panel.style.overflow='hidden'
         panel.style.opacity = '0'
         visible_panel=1
     }
     else{
-        panel.style.display='block'
+        
         panel.style.visibility='visible'
         panel.style.overflow='visible'
         panel.style.opacity = '1'
         visible_panel=0
     }
 
-    
+    e.preventDefault()
     panel.style.transition= 'ease 0.5s all'
 
 })
